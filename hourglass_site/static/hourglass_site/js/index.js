ed6f851bbe4a8682f89184d9ecbf53f687b13a49
Brian DeRocher <brian@derocher.org>
2015-05-17 00:57:17 -0400

    Add an NVD3 based chart.

--- hourglass_site/static/hourglass_site/js/index.js
+++ hourglass_site/static/hourglass_site/js/index.js
@@ -215,11 +215,13 @@
     if (res && res.results && res.results.length) {
       // updatePriceRange(res);
       updatePriceHistogram(res);
+      updateChart(res);
       updateResults(res);
     } else {
       res = EMPTY_DATA;
       // updatePriceRange(EMPTY_DATA);
       updatePriceHistogram(res);
+      updateChart(res);
       updateResults(res);
     }
   }
@@ -431,6 +433,108 @@
 
     histogramUpdated = true;
   }
+  
+  function updateChart(data) {
+    console.log( data );
+    nv.addGraph(function() {
+      var chart = nv.models.multiBarChart()
+        .showControls(false);
+
+      chart.xAxis.tickFormat(d3.format(',f'));
+
+      chart.yAxis.tickFormat(d3.format(',.1f'));
+
+      var data = getData2();
+      console.log( 'data is' );
+      console.log( data );
+      d3.select('#chart svg')
+          .datum(data)
+          .call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+
+    function getData() {
+      return stream_layers(2,10+Math.random()*100,.1).map(function(data, i) {
+        return {
+          key: 'Stream #' + i,
+          values: data
+        };
+      });
+    }
+    
+    function getData2() {
+      return [
+               { key: "Stream 0"
+               , values: 
+                    [    {series: 0, x: 0, y: 1.2}
+                    ,    {series: 0, x: 1, y: 5.2}
+                    ,    {series: 0, x: 2, y: 8.2}
+                    ,    {series: 0, x: 3, y: 6.2}
+                    ,    {series: 0, x: 4, y: 5.2}
+                    ,    {series: 0, x: 5, y: 4.2}
+                    ,    {series: 0, x: 6, y: 3.2}
+                    ,    {series: 0, x: 7, y: 2.2}
+                    ,    {series: 0, x: 8, y: 2.2}
+                    ,    {series: 0, x: 9, y: 1.2}
+                    ]
+               }
+          ,    { key: "Stream 1"
+               , values: 
+                    [    {series: 1, x: 0, y: 1.2}
+                    ,    {series: 1, x: 1, y: 2.2}
+                    ,    {series: 1, x: 2, y: 3.2}
+                    ,    {series: 1, x: 3, y: 4.2}
+                    ,    {series: 1, x: 4, y: 4.2}
+                    ,    {series: 1, x: 5, y: 4.2}
+                    ,    {series: 1, x: 6, y: 5.2}
+                    ,    {series: 1, x: 7, y: 7.2}
+                    ,    {series: 1, x: 8, y: 8.2}
+                    ,    {series: 1, x: 9, y: 6.2}
+                    ]
+               }
+          ];
+    }
+
+  }
+
+/* Inspired by Lee Byron's test data generator. */
+function stream_layers(n, m, o) {
+  if (arguments.length < 3) o = 0;
+  function bump(a) {
+    var x = 1 / (.1 + Math.random()),
+        y = 2 * Math.random() - .5,
+        z = 10 / (.1 + Math.random());
+    for (var i = 0; i < m; i++) {
+      var w = (i / m - y) * z;
+      a[i] += x * Math.exp(-w * w);
+    }
+  }
+  return d3.range(n).map(function() {
+      var a = [], i;
+      for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+      for (i = 0; i < 5; i++) bump(a);
+      return a.map(stream_index);
+    });
+}
+
+/* Another layer generator using gamma distributions. */
+function stream_waves(n, m) {
+  return d3.range(n).map(function(i) {
+    return d3.range(m).map(function(j) {
+        var x = 20 * j / m - i / 3;
+        return 2 * x * Math.exp(-.5 * x);
+      }).map(stream_index);
+    });
+}
+
+function stream_index(d, i) {
+  return {x: i, y: Math.max(0, d)};
+}
+
+
 
   function updateResults(data) {
     var results = data.results;
